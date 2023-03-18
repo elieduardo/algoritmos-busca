abfa33370dc050717091146c84612d696937470b
Eduardo Eli <eduardoely@live.com>
2023-03-18 11:50:07 -0300

    criado projeto base e incio de desenvolvimento e organização do projeto.

---
+++ algoritmos-busca/MetodosDeBusca.cs
@@ -0,0 +1,40 @@
+﻿namespace algoritmos_busca
+{
+    static class MetodosDeBusca
+    {
+        static int BuscaLinear(this IList<int> lista, int elementoProcurado)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i] == elementoProcurado)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        static int BuscaLinearOrdenada(this IList<int> lista, int elementoProcurado)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i] == elementoProcurado)
+                {
+                    return i;
+                }
+                else if (elementoProcurado < lista[i])
+                {
+                    return -1;
+                }
+            }
+
+            return -1;
+        }
+
+        static int BuscaBinaria(this IList<int> lista, int elementoProcurado)
+        {
+            return -1;
+        }
+    }
+}
---
+++ algoritmos-busca/Uteis.cs
@@ -0,0 +1,16 @@
+﻿namespace algoritmos_busca
+{
+    static class Uteis
+    {
+
+        static IList<int> OrdenarLista(this IList<int> lista)
+        {
+            return lista.OrderBy(x => x).ToList();
+        }
+
+        static IList<int> DivideLista(this IList<int> lista, int elementoProcurado)
+        {
+            return lista;
+        }
+    }
+}
